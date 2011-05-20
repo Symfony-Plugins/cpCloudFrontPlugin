5f6792bbfc7698c708e1797829dad4dc5c1daf85
coopers <coopers@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2011-05-20 07:39:45 -0500

    Merge branch 'master' into svn
    git-svn-id: http://svn.symfony-project.com/plugins/cpCloudFrontPlugin/trunk@32565 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/helper/CloudfrontHelper.php
@@ -0,0 +1,23 @@
+<?php
+
+function _cloudfront_base_url() {
+  $prefix = sfConfig::get('app_cloudfront_images_prefix', '/images');
+  if ($prefix && substr($prefix, -1) != '/') { $prefix .= '/'; }
+  return sfConfig::get('app_cloudfront_url') . $prefix;
+}
+
+function cloudfront_image_tag($source, $options = array()) {
+  $enabled = true; //sfConfig::get('app_cloudfront_enable', false);
+  $src = ($enabled ? _cloudfront_base_url() : '') . $source;
+  return image_tag($src, $options);
+}
+
+function cloudfront_image_path($source) {
+  $enabled = sfConfig::get('app_cloudfront_enable', false);
+  return $enabled ? _cloudfront_base_url() . $source : image_path($source);
+}
+
+function cloudfront_submit_image_tag($source, $options = array()) {
+  $enabled = sfConfig::get('app_cloudfront_enable', false);
+  return submit_image_tag($enabled ? _cloudfront_base_url() . $source : $source, $options);
+}
